6aa8068faa20acb34254d1e2ae2c62b597449201
Rakshith Kumar Lokesh Rao <rakshithkumar.lr@gmail.com>
2024-02-27 09:45:40 -0700

    Added Kubernetes deployment manifests files

--- python_services/src/gateway/storage/util.py
+++ python_services/src/gateway/storage/util.py
@@ -4,7 +4,7 @@ def upload(f, fs, channel, access):
     try:
         fid =  fs.put(f)
     except Exception as err:
-        return "Internal Server Error", 500
+        return f"Error: ", err
     
     message = {
         "video_fid": str(fid),
@@ -21,6 +21,6 @@ def upload(f, fs, channel, access):
                 delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE  # To make the queue and its messages durable in the event of the failure of the pod
             ),
         )
-    except:  # If that doesn't work, we have to delete the file from mongodb because if there is no message on the queue for the file and the file does exist on mongodb then the file will never get processed because down stream service does not know that the file exists if it never receive receives a message telling it to process. We will end up having stale files in mongodb if we don't delete them in the event if we can't put the message onto the queue.
+    except Exception as err:  # If that doesn't work, we have to delete the file from mongodb because if there is no message on the queue for the file and the file does exist on mongodb then the file will never get processed because down stream service does not know that the file exists if it never receive receives a message telling it to process. We will end up having stale files in mongodb if we don't delete them in the event if we can't put the message onto the queue.
         fs.delete(fid)
-        return "Internal Server Error", 500
+        return f"Error: ", err
